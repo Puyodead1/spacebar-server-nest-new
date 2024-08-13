968e8c15078f806d7f63686ffd6618e873cb59b2
Puyodead1 <puyodead@proton.me>
2024-08-13 09:11:28 -0400

    url versioning

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+import { VERSION_NEUTRAL, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableVersioning({
+    type: VersioningType.URI,
+    prefix: false,
+    defaultVersion: VERSION_NEUTRAL,
+  });
   await app.listen(3000);
 }
 bootstrap();
